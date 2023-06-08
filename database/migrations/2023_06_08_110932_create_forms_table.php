68714c2f6a15ae30a2dda2c7e8a33be557b2a030
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2023-06-09 02:55:04 +0300

    fix

--- database/migrations/2023_06_08_110932_create_forms_table.php
+++ database/migrations/2023_06_08_110932_create_forms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFormSubmissionsTable extends Migration
+class CreateFormsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,23 @@ class CreateFormSubmissionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('form_submissions', function (Blueprint $table) {
-            $table->increments('id');
-
-            $table->unsignedInteger('form_id');
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
 
             $table->unsignedBigInteger('user_id')->nullable();
 
-            $table->text('content');
+            $table->string('name');
+            $table->string('visibility');
+            $table->boolean('allows_edit')->default(false);
 
+            $table->string('identifier')->unique();
+            $table->text('form_builder_json')->nullable();
+
+            $table->string('custom_submit_url')->nullable();
+
+            $table->softDeletes();
             $table->timestamps();
 
-            $table->foreign('form_id')->references('id')->on('forms')->onDelete('CASCADE');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
         });
     }
@@ -36,6 +41,6 @@ class CreateFormSubmissionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('form_submissions');
+        Schema::dropIfExists('forms');
     }
 }
